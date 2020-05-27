b97edb8a6ad94905d912fdba52e18eadd6cfe157
Tujiawang <wanggaungxi88@gamil.com>
2020-05-27 16:02:33 +0800

    新增函数节流，防止重复点击

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -1,12 +1,16 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const utils = require('../../utils/util')
 Page({
   data: {
-    
+
   },
   onLoad: function () {
-    
-  }
+
+  },
+  tap: utils.throttle((e) => {
+    // e，为传递过来的参数
+    // 需要处理的逻辑
+  }, 1000)
 })
--- demo/utils/util.js
+++ demo/utils/util.js
@@ -13,7 +13,30 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+ 
+/**
+ * 函数节流，防止重复点击
+ * @param {*} fn 
+ * @param {*} gapTime 
+ */
+const throttle = (fn, gapTime) => {
+  if (gapTime == null || gapTime == undefined) {
+    gapTime = 1500
+  }
+
+  let _lastTime = null
+
+  // 返回新的函数
+  return function () {
+    let _nowTime = +new Date()
+    if (_nowTime - _lastTime > gapTime || !_lastTime) {
+      fn.apply(this, arguments) //将this和参数传给原函数
+      _lastTime = _nowTime
+    }
+  }
+}
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  throttle
 }
