6c1135d5c9c8348029257ebec1d304140adcf743
Tujiawang <wanggaungxi88@gamil.com>
2020-05-29 15:42:48 +0800

    新增选择当前位置和打开导航

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 const utils = require('../../utils/util')
 Page({
   data: {
-
+    address: ''
   },
   onLoad: function () {
 
@@ -12,5 +12,21 @@ Page({
   tap: utils.throttle((e) => {
     // e，为传递过来的参数
     // 需要处理的逻辑
-  }, 1000)
+  }, 1000),
+  chooseLocation() {
+    app.authLocation(() => {
+      wx.chooseLocation({
+        success: (res) => {
+          if (res.address) {
+            this.setData({
+              address: res.address
+            })
+          }
+        },
+      })
+    })
+  },
+  openLocation(e) {
+    app.openLocation(e.currentTarget.dataset.lat, e.currentTarget.dataset.lot)
+  },
 })
